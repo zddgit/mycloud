d69ba51b00bc32370ad4fc0c188ac8b27c8e0f45
zdd <583627140@qq.com>
2019-03-24 11:11:34 +0800

    添加一些注释。添加ribbon对应用的执行策略

--- mycloud-service/src/main/java/com/zdd/service/IUserService.java
+++ mycloud-service/src/main/java/com/zdd/service/IUserService.java
@@ -1,11 +1,11 @@
 package com.zdd.service;
 
+import com.zdd.service.feign.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "MYCLOUD-PRODUCT",configuration = FeignClientConfig.class) //由于没有启用登录功能所以没有启用登陆配置
-@FeignClient(name = "MYCLOUD-PRODUCT")
+@FeignClient(name = "MYCLOUD-PRODUCT",configuration = FeignClientConfig.class)
 public interface IUserService {
     @GetMapping("/product/user/{userid}")
     public Object getUser(@PathVariable("userid") Integer userid);
--- mycloud-service/src/main/java/com/zdd/service/feign/FeignClientConfig.java
+++ mycloud-service/src/main/java/com/zdd/service/feign/FeignClientConfig.java
@@ -1,12 +1,18 @@
 package com.zdd.service.feign;
 
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-
+//feign在调用的时候应用的一些配置如：登录，日志级别等等
 @Configuration
 public class FeignClientConfig {
     //添加安全验证，登录名和密码
-    public BasicAuthenticationInterceptor getBasicAuthenticationInterceptor(){
-        return new BasicAuthenticationInterceptor("admin","enjoy");
+//    public BasicAuthenticationInterceptor getBasicAuthenticationInterceptor(){
+//        return new BasicAuthenticationInterceptor("admin","enjoy");
+//    }
+    @Bean
+    public Logger.Level getFeignLoggerLevel() {
+        return feign.Logger.Level.FULL ;
     }
 }
